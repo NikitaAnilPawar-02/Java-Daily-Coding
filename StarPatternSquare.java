43fad18bf22c70bc7dd1acc8a212d6c1ef306523
NIKITA <pawarnikita0293@gmail.com>
2025-12-30 12:43:29 +0530

    Fix logical operator in star pattern condition

--- StarPatternSquare.java
+++ StarPatternSquare.java
@@ -9,7 +9,7 @@ class StarPatternSquare{
 
 		for(int i = 1; i<=row; i++){
 			for(int j=1; j<=col; j++){
-				if(i==1 | j ==1 || i==row || j==col){
+				if(i==1 || j ==1 || i==row || j==col){
 					System.out.print("* ");
 				}else{
 					System.out.print("  ");
